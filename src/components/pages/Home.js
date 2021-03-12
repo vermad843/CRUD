27a5e49de6efec2cbd14ac071f042646803a44eb
vermad843 <vishnuverma7524@gmail.com>
2021-03-13 03:34:02 +0530

    table on the page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -17,6 +17,37 @@ const Home = () => {
         <div className ="container">
            <div className = "py-4">
               <h1>Home Page</h1>
+              <table className="table border shadow">
+                <caption>List of users</caption>
+                <thead className="table-dark">
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">3</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@twitter</td>
+                  </tr>
+                </tbody>
+              </table>
            </div>         
         </div>
     )
