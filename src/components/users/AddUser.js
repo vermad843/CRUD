f24b2560313135da069b26652ed2bd97338fdc07
vermad843 <vishnuverma7524@gmail.com>
2021-03-13 04:49:53 +0530

    adding user to the table

--- src/components/users/AddUser.js
+++ src/components/users/AddUser.js
@@ -1,17 +1,45 @@
-import React from 'react'
+import axios from 'axios';
+import React, {useState} from 'react';
+import {useHistory} from 'react-router-dom';
 
 const AddUser = () => {
+    const history = useHistory();
+    const [user, setUser] = useState({
+         name  : '',
+         username : '',
+         email : '',
+         phone : '',
+         website : ''
+    });
+    
+    const {name, username, email, phone, website} = user;
+
+    const onInputChange = (e) => {
+        setUser({
+            ...user,
+            [e.target.name] : e.target.value
+        })
+    };
+
+    const onSubmit = async(e) => {
+      e.preventDefault();
+      await axios.post('http://localhost:3003/users', user); 
+      history.push('/')
+    };
+
     return (
         <div className = "container">
            <div className = "w-75 mx-auto shadow p-5">
               <h2 className = "text-center mb-4">Add A User</h2>
-             <form>
+             <form onSubmit = {(e) => onSubmit(e)}>
                  <div className = "form-group">
                     <input
                         type = "text"
                         className = "form-control form-control-lg"
                         placeholder="Enter Your Name"
                         name = "name"
+                        value = {name}
+                        onChange = {(e) => onInputChange(e)}
                     />
                  </div>
                  <div className = "form-group">
@@ -20,6 +48,8 @@ const AddUser = () => {
                         className = "form-control form-control-lg"
                         placeholder="Enter Your Username"
                         name = "username"
+                        value = {username}
+                        onChange = {(e) => onInputChange(e)}
                     />
                  </div>
                  <div className = "form-group">
@@ -28,6 +58,8 @@ const AddUser = () => {
                         className = "form-control form-control-lg"
                         placeholder="Enter Your E-mail Address"
                         name = "email"
+                        value = {email}
+                        onChange = {(e) => onInputChange(e)}
                     />
                  </div> 
                  <div className = "form-group">
@@ -36,6 +68,8 @@ const AddUser = () => {
                         className = "form-control form-control-lg"
                         placeholder="Enter Your Phone Number"
                         name = "phone"
+                        value = {phone}
+                        onChange = {(e) => onInputChange(e)}
                     />
                  </div>
                  <div className = "form-group">
@@ -44,6 +78,8 @@ const AddUser = () => {
                         className = "form-control form-control-lg"
                         placeholder="Enter Your Website Name"
                         name = "website"
+                        value = {website}
+                        onChange = {(e) => onInputChange(e)}
                     />
                  </div>
                  <button className = "btn btn-primary btn-block">Add User</button>
