a6f908f1b618ed866aa01ee1eba6ab297915dec3
vermad843 <vishnuverma7524@gmail.com>
2021-03-13 01:16:56 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Contact from './components/pages/Contact';
+import Navbar from './layout/Navbar'
 
 
 function App() {
   return (
     <div className="App">
-       <h1>Hello World!!!</h1>
+       <Navbar/>
        <Home/>
        <About/>
        <Contact/>
