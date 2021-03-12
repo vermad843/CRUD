6f9bac1dd3a12b3aceac52f84bbb7bd1238843fb
vermad843 <vishnuverma7524@gmail.com>
2021-03-13 02:30:00 +0530

    not found page

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -5,24 +5,24 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
             <div className = "container">
-              <a className="navbar-brand" href="#">Navbar</a>
+              <a className="navbar-brand" href="#">Recat User</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                   <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse">
                   <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
-                      <NavLink className="nav-link" to="/">
+                      <NavLink className="nav-link" exact to="/">
                         Home
                       </NavLink>
                     </li>
                     <li className="nav-item">
-                      <NavLink className="nav-link" to="/about">
+                      <NavLink className="nav-link" exact to="/about">
                         About
                       </NavLink>
                     </li>
                     <li className="nav-item">
-                      <NavLink className="nav-link" to="/contact">
+                      <NavLink className="nav-link" exact to="/contact">
                         Contact
                       </NavLink>
                     </li>
