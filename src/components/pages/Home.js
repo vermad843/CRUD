51407445671eacdef1b707d2568bfd4d13e3cc25
vermad843 <vishnuverma7524@gmail.com>
2021-03-13 03:42:50 +0530

    showing the json data on the page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -22,30 +22,23 @@ const Home = () => {
                 <thead className="table-dark">
                   <tr>
                     <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">User Name</th>
+                    <th scope="col">Email</th>
+                    <th>Action</th>
                   </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
-                  </tr>
+                     {
+                        users.map((user, i) => (
+                           <tr>
+                              <th scope="row">{i + 1}</th>
+                              <td >{user.name}</td>
+                              <td >{user.username}</td>
+                              <td >{user.email}</td>
+                           </tr>
+                        ))
+                     }
                 </tbody>
               </table>
            </div>         
